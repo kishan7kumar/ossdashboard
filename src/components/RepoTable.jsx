2d08729d6b3a8b8b58c524566b727640018287f8
kishan7kumar <kishankumar3202@gmail.com>
2023-05-30 16:22:39 +0530

    Add table

---
+++ src/components/RepoTable.jsx
@@ -0,0 +1,80 @@
+import { Space, Table, Tag, Button } from "antd";
+import * as dayjs from "dayjs";
+
+const columns = [
+  {
+    title: "Company",
+    dataIndex: "companyName",
+    key: "companyName",
+    render: (text) => (
+      <a href={text[1]} target="blank">
+        {text[0]}
+      </a>
+    ),
+  },
+  {
+    title: "GitHub",
+    key: "details",
+    dataIndex: "githubLink",
+    width: 120,
+    render: (record) => (
+      <Button size="small" block onClick={() => navigateToRepo(record)}>
+        Repo
+      </Button>
+    ),
+  },
+  {
+    title: "Founded",
+    dataIndex: "founded",
+    key: "founded",
+    sorter: (a, b) => a.founded - b.founded,
+    render: (date) => (
+      <p className="w-full m-0 text-end"> {dayjs(date).format("YYYY")}</p>
+    ),
+    width: 120,
+  },
+
+  {
+    title: "Stars",
+    dataIndex: "stars",
+    key: "stars",
+    sorter: (a, b) => a.stars - b.stars,
+    render: (stars) => (
+      <p className="w-full m-0 text-end"> {stars.toLocaleString()}</p>
+    ),
+    width: 120,
+  },
+
+  {
+    title: "Forks",
+    dataIndex: "forks",
+    key: "forks",
+    sorter: (a, b) => a.forks - b.forks,
+    render: (forks) => (
+      <p className="w-full m-0 text-end"> {forks.toLocaleString()}</p>
+    ),
+    width: 120,
+  },
+];
+
+const navigateToRepo = (linkToRepo) => {
+  window.open(linkToRepo, "_blank");
+};
+
+const RepoTable = ({ data }) => {
+  return (
+    <div className="w-full">
+      <Table
+        loading={data.length > 0 ? false : true}
+        bordered
+        columns={columns}
+        size="small"
+        pagination={{ pageSize: 25 }}
+        dataSource={data}
+        scroll={true}
+      />
+    </div>
+  );
+};
+
+export default RepoTable;
--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,17 +1,50 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router";
-import { Button, Tooltip, Space } from "antd";
+import { Button, Tooltip, Space, Radio } from "antd";
 import {
   MdKeyboardArrowLeft,
   MdFullscreen,
   MdFullscreenExit,
 } from "react-icons/md";
+import { BsStars } from "react-icons/bs";
+import request from "../utils/request";
+import RepoTable from "../components/RepoTable";
 
 const DashboardPage = () => {
   const navigate = useNavigate();
   const [isFullScreen, setIsFullScreen] = useState(false);
+  const [tableData, setTableData] = useState([]);
   let toggle = false;
   var elem = document.documentElement;
+
+  const getAllEventsList = async () => {
+    try {
+      const data = await request(
+        "/search/repositories?q=stars:>=1000&per_page=50&page=1",
+        {
+          method: "GET",
+        }
+      );
+      let newdata = [];
+      // console.log(data.items);
+      data.items.forEach((element, index) => {
+        newdata.push({
+          key: index,
+          companyName: [element.owner.login, element.homepage],
+          stars: element.stargazers_count,
+          repoName: element.full_name,
+          githubLink: element.html_url,
+          forks: element.forks_count,
+          founded: element.created_at,
+        });
+      });
+      // console.log(newdata);
+      setTableData(newdata);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   const toggleFullScreen = () => {
     if (!isFullScreen) {
       elem.requestFullscreen();
@@ -21,6 +54,11 @@ const DashboardPage = () => {
       setIsFullScreen(false);
     }
   };
+
+  useEffect(() => {
+    getAllEventsList();
+  }, []);
+
   return (
     <div className="h-full w-full flex flex-col bg-gray-100">
       <div className="h-14 bg-blue-600 drop-shadow-lg p-2 flex items-center justify-between">
@@ -50,8 +88,29 @@ const DashboardPage = () => {
           />
         </div>
       </div>
-      <div className="grow">
-        <div className="h-full w-full overflow-y-auto"></div>
+      <div className="grow min-h-0">
+        <div className="h-full w-full overflow-y-auto flex p-4">
+          <div className="container m-auto h-full">
+            <div className="my-6 flex justify-between flex-wrap">
+              <div className=" flex items-center">
+                <BsStars className="text-amber-400 text-3xl" />
+                <span className="text-gray-700 text-2xl font-medium ml-2">
+                  Popular 50 Open Source GitHub Repos as of 2023
+                </span>
+              </div>
+              <Radio.Group
+                defaultValue="table"
+                buttonStyle="solid"
+                className="mt-4 lg:mt-0"
+              >
+                <Radio.Button value="table">Table View</Radio.Button>
+                <Radio.Button value="chart">Chart View</Radio.Button>
+              </Radio.Group>
+            </div>
+
+            <RepoTable data={tableData}></RepoTable>
+          </div>
+        </div>
       </div>
     </div>
   );
